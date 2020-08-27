b642ac113da4c9b39d8034d2270fa4efec41045a
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-08-27 20:03:55 +0300

    add banner without pixelperfect

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import './banner.scss'
+import React from 'react';
+import './banner.scss';
+import Button from '../Buttons/Button';
 
 const Banner = () => {
-    return (
-        <div className="banner img-fluid">
-            
+  return (
+    <div className="banner">
+      <div className="banner__infoContainer">
+        <div className="banner__title">
+          <div>
+            <span>Test assignment</span>
+          </div>
+          <div>
+            <span>for Frontend</span>
+          </div>
+          <div>
+            <span>Developer position</span>
+          </div>
         </div>
-    )
-}
+        <div className="banner__contentMobile text-white d-sm-none">
+          <div>
+            <span>We kindly remind you that your test </span>
+          </div>
+          <div>
+            <span> assignment should be submitted as a link to </span>
+          </div>
+          <div>
+            <span>github/bitbucket repository.</span>
+          </div>
+        </div>
+        <div className="bannerContent d-none d-sm-block text-white">
+          We kindly remind you that your test assignment should be submitted as
+          a link to github/bitbucket repository. Please be patient, we consider
+          and respond to every application that meets minimum requirements. We
+          look forward to your submission. Good luck! The photo has to scale in
+          the banner area on the different screens
+        </div>
+        <Button text="Sign up now" />
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
