c03f0b34d2d352eeef6b5ee3985e13aa2aec1fe9
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-08-29 22:50:40 +0300

    Updates

---
+++ precache-manifest.f0f3523b708a1f2f29d53e8a0466f278.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ece27df3cb1d60581b06bd411bfc39c",
+    "url": "/testingtask/index.html"
+  },
+  {
+    "revision": "c46911d60631471be465",
+    "url": "/testingtask/static/css/main.75e892a8.chunk.css"
+  },
+  {
+    "revision": "0938a7c07c07d53d1a71",
+    "url": "/testingtask/static/js/2.0b5df126.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/testingtask/static/js/2.0b5df126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46911d60631471be465",
+    "url": "/testingtask/static/js/main.0f202781.chunk.js"
+  },
+  {
+    "revision": "9e42e03520f7349e0718",
+    "url": "/testingtask/static/js/runtime-main.5e1690a6.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/testingtask/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/testingtask/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/testingtask/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/testingtask/static/media/menu-icon.6f6bef28.svg"
+  }
+]);
